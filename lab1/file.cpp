871d2bb444ee59b0f685c1b39bb54e9f5bc91cbf
Basti <basti.babaeva@yandex.ru>
2024-09-30 21:36:27 +0700

    Добавление и определение методов isEmpty(), isExist(), isChanged(), isChangedStatus(), isChangedSize() в классе File

--- lab1/file.cpp
+++ lab1/file.cpp
@@ -6,3 +6,36 @@ File::File(const QString& name):fileName(name) //определение конс
     size = fileInfo.size(); //вызов метода size() для нахождения размера файла
     status = fileInfo.exists(); //вызов метода size() для определения факта существования файла
 }
+bool File::isExist() //проверка на существование файла
+{
+    QFileInfo file(fileName);
+    return file.exists(); //если true, файл существует
+}
+
+bool File::isEmpty() //проверка на пустоту файла
+{
+    QFileInfo file(fileName);
+    if ((file.exists() == true) && (file.size() == 0)) return true; //если файл существует и его размер = 0, то true
+    else return false;
+}
+
+bool File::isChangedAll() //проверка на изменение размера и существования файла
+{
+    QFileInfo file(fileName);
+    if (file.exists() != status || file.size() != size) return true;
+    else return false;
+}
+
+bool File::isChangedStatus() //проверка на изменение существования файла
+{
+    QFileInfo file(fileName);
+    if (file.exists() != status) return true;
+    else return false;
+}
+
+bool File::isChangedSize() //проверка на изменение размера файла
+{
+    QFileInfo file(fileName);
+    if (file.size() != size) return true;
+    else return false;
+}
--- lab1/file.h
+++ lab1/file.h
@@ -9,11 +9,13 @@ class File: public QFileInfo //класс File, наследуемый от кл
     bool status; //статус - существование файла
 
 public:
-    File(const QString& name = "NoFile"); //конструктор с параметром по умолчанию
+    File(const QString& name); //конструктор с параметром по умолчанию
 
     bool isExist(); //проверка на существование файла
     bool isEmpty(); //проверка на пустоту файла
-    bool isChanged(); //проверка на изменение файла
+    bool isChangedAll(); //проверка на изменение размера и существования файла
+    bool isChangedStatus(); //проверка на изменение существования файла
+    bool isChangedSize(); //проверка на изменение размера файла
 };
 
 #endif // FILE_H
